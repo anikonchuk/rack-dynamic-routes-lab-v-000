4790171987bfdb40b9b5a74731bc23ccb6b3ffbe
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-25 16:35:40 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      resp.write "Test"
+    else
+      resp.write "Path not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
